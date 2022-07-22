ddf6a7b2ed366156802bead3399dfd756f13d5c1
arcbsr <bsrsoftbd@gmail.com>
2022-07-22 22:22:45 +0600

    wewe

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -62,7 +62,7 @@ userSchema.pre("save", async function (next) {
 
 // jwt token
 userSchema.methods.getJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+  return jwt.sign({ id: this._id }, "bezkoder-secret-key", {
     expiresIn:  process.env.JWT_EXPIRES,
   });
 };
